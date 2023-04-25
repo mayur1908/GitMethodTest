1032e46ab381863f5d9a75c6e0904ecd2e3eabf7
Mayur Sonkusare <mayursonkusare1155@gmail.com>
2023-04-25 08:57:17 +0530

    added Program.cs file

---
+++ Method/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*  Console.WriteLine("Enter the Number");
+              int val1 = Convert.ToInt32(Console.ReadLine());
+              int val2 = Convert.ToInt32(Console.ReadLine());
+              Console.WriteLine("Enter 1) addition \n 2) Subtraction \n 3)Multiplication \n 4) Division");
+              int num1=Convert.ToInt32(Console.ReadLine());
+              if (num1 == 1) 
+              {
+                 int ans = UserNameDisplay.DisplayAdd(val1, val2);
+                 Console.WriteLine(ans);
+              }
+              else if (num1 == 2)
+              {
+                  Console.WriteLine(UserNameDisplay.DisplayDif(val1,val2));
+              }
+              else if (num1 == 3)
+              {
+                  Console.WriteLine(UserNameDisplay.DisplayProd(val1, val2));
+              }
+              else if (num1 == 4)
+              {
+                  Console.WriteLine(UserNameDisplay.DisplayDiv(val1, val2));
+              }
+              else
+              {
+                  Console.WriteLine("Invalid Input");
+              }
+            
+            FlipCoin.DisplayPercentage();
+            */
+            LeapYear.Lyear();
+            Console.ReadKey();
+        }
+    }
+}
